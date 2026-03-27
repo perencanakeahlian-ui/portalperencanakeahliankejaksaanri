da61fb78fbe16a0634413218c651ea3d6ba9d72d
Perencana Keahlian <admin@perencanakeahlian-ui.com>
2026-03-27 11:38:47 +0900

    feat: migration to Astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,6 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://perencanakeahlian-ui.github.io',
-  base: '/portalperencanakeahliankejaksaanri',
   vite: {
     plugins: [tailwindcss()],
   },
